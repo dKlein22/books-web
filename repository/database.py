5fb1da7ac453c76e9dc6e9a179a6073152c1f3a6
Dominic Klein <kleindominic39@gmail.com>
2026-08-11 23:28:12 -0300

    Create database, connect db with py

---
+++ repository/database.py
@@ -0,0 +1,24 @@
+import sqlite3
+from pathlib import Path
+
+def get_connection():
+
+    connection = sqlite3.connect("data/books.db")
+    connection.execute("PRAGMA foreign_keys = ON")
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def init_schema(connection):
+
+    schema_path = Path(__file__).parent / "schema.sql"
+    with open(schema_path, "r") as file:
+        cursor = connection.cursor()
+        cursor.executescript(file.read())
+        connection.commit()
+
+def run_seed(connection):
+    seed_path = Path(__file__).parent / "seed.sql"
+    with open(seed_path, "r") as file:
+        cursor = connection.cursor()
+        cursor.executescript(file.read())
+        connection.commit()
