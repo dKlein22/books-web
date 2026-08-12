c09f4acf79071719f31ffd19153da70f596d24a5
Dominic Klein <kleindominic39@gmail.com>
2026-08-11 23:18:52 -0300

    Inicio do projeto

---
+++ repository/schema.sql
@@ -0,0 +1,25 @@
+--.schema books data base
+
+CREATE TABLE IF NOT EXISTS books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    genre TEXT NOT NULL,
+    year INTEGER NOT NULL,
+    rating REAL NOT NULL,
+    author TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS favorites (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_book INTEGER NOT NULL,
+    id_user INTEGER NOT NULL,
+    FOREIGN KEY(id_book) REFERENCES books(id),
+    FOREIGN KEY(id_user) REFERENCES users(id)
+);
