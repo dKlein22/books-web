c796f607dadc71d4f5cacaed7ec1623d15e3c6c9
Dominic Klein <kleindominic39@gmail.com>
2026-08-12 22:58:35 -0300

    Instance flask class

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from flask import Flask, render_template, request, session
 from repository.database import  get_connection, init_schema
 
 connection = get_connection()
 init_schema(connection)
+
+app = Flask(__name__)
 
 connection.close()
