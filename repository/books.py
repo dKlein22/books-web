7356b093bf96000fea5ff5cc13e1c371c09a1b95
Dominic Klein <kleindominic39@gmail.com>
2026-08-12 22:45:29 -0300

    Create the files users, books and favorites

---
+++ repository/books.py
@@ -0,0 +1,35 @@
+def search_id(connection, id):
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM books WHERE id = ?", (id,))
+    return cursor.fetchone()
+
+def search_title(connection, title):
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM books WHERE title LIKE ?", (f"%{title}%",))
+    return cursor.fetchall()
+
+def search_genre(connection, genre):
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM books WHERE genre LIKE ?", (f"%{genre}%",))
+    return cursor.fetchall()
+
+def search_rating(connection, min_rating, max_rating):
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM books WHERE rating BETWEEN ? AND ?", (min_rating, max_rating))
+    return cursor.fetchall()
+
+def search_author(connection, author):
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM books WHERE author LIKE ?", (f"%{author}%",))
+    return cursor.fetchall()
+
+def search_top10(connection):
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM books ORDER BY rating DESC LIMIT 10")
+    return cursor.fetchall()
---
+++ repository/users.py
@@ -0,0 +1,12 @@
+def create_user(connection, name, email, password_hash):
+    cursor = connection.cursor()
+    cursor.execute(
+        "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+        (name, email, password_hash)
+    )
+    connection.commit()
+
+def get_user_email(connection, email):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
+    return connection.fetchone()
