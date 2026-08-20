82631e5c3d99781e82086ffb3064dc0487e6a981
Dominic Klein <kleindominic39@gmail.com>
2026-08-20 17:07:17 -0300

    Create two routes, favorites and del favorites

--- routes/books.py
+++ routes/books.py
@@ -2,7 +2,7 @@ from authorization.required import login_required
 from flask import Blueprint, request, render_template, redirect, session, url_for
 from repository.books import search_title
 from repository.database import get_db
-from repository.favorites import add_favorite
+from repository.favorites import add_favorite, del_favorite, list_favorites
 
 books_bp = Blueprint("books", __name__)
 
@@ -29,4 +29,28 @@ def add_favorites():
     add_favorite(connection, book_id, user_id)
 
     return redirect(url_for("home.home"))
+
+@books_bp.route("/favorites")
+@login_required
+def favorites():
+
+    connection = get_db()
+    user_id = session['user_id']
+    results = list_favorites(connection, user_id)
+
+    return render_template("favorites.html", results=results)
+
+@books_bp.route("/del_favorite", methods=["POST"])
+@login_required
+def del_favorites():
+
+    book_id = int(request.form.get("book_id"))
+    user_id = session['user_id']
+
+    connection = get_db()
+
+    del_favorite(connection, book_id, user_id)
+
+    return redirect(url_for("books.favorites"))
+
     
