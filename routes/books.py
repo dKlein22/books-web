3310b1f43fd9a77ad94c971f9442a7c358013817
Dominic Klein <kleindominic39@gmail.com>
2026-08-19 23:31:43 -0300

    Create route and template

--- routes/books.py
+++ routes/books.py
@@ -1,7 +1,8 @@
-from authorizations import login_required
-from flask import Blueprint, request, render_template, redirect, session
+from authorizations.required import login_required
+from flask import Blueprint, request, render_template, redirect, session, url_for
 from repository.books import search_title
 from repository.database import get_db
+from repository.favorites import add_favorite
 
 books_bp = Blueprint("books", __name__)
 
@@ -14,4 +15,15 @@ def search():
 
     results = search_title(connection, title) if title else []
 
-    return render_template("searcH.html", results=results)
+    return render_template("search.html", results=results)
+
+@books_bp.route("/add_favorite", methods=["POST"])
+def add_favorite():
+
+    book_id = request.form.get("book_id")
+    user_id = session['user_id']
+
+    add_favorite(get_db, book_id, user_id)
+
+    return redirect(url_for("books.search"))
+    
